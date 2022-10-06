dd81c69db78569f6d694f15655032e2df9bc7240
ABHISHEK HATKAR <abhihatkar2000ak@gmail.com>
2022-10-06 18:13:06 +0530

    skeleton

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pyexpat import model
 from flask import Flask, render_template,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ class user(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(80), nullable=False)
-
+    
 @app.route("/")
 def home():
     return render_template('frontpage.html')
@@ -22,17 +23,28 @@ def home():
 def AdminLogin():
     return render_template('admin_login.html')
 
+@app.route("/admin_register")
+def AdminRegister():
+    return render_template('admin_register.html')
+
 @app.route("/hr_login")
 def HrLogin():
     return render_template('hr_login.html')
-
-@app.route("/admin_register")
-def AdminRegister():
-    return render_template('admin_login.html')
 
 @app.route("/hr_register")
 def HrRegister():
     return render_template('hr_login.html')
 
+@app.route("/admin_page")
+def AdminPage():
+    return render_template('adminpage.html')
+
+@app.route("/hr_page")
+def HrPage():
+    return render_template('hrpage.html')
+
+@app.route("/hrsearchpage")
+def HrSearchPage():
+    return render_template('hrsearchpage.html')
 if __name__ == '__main__':
     app.run(debug=True)
